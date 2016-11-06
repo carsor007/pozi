0ac377a3b58f7b6bdf488d2606e67d715f113f48
Sam Njoroge <samkasa007@yahoo.com>
2016-11-06 12:29:09 -0600

    removed sphinx  Please enter the commit message for your changes. Lines starting

--- build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to nangos&#8217;s documentation!"],titleterms:{document:0,indice:0,nango:0,tabl:0,welcom:0}})
